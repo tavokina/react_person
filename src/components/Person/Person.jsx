b3279bc9a4ea387dbe9aa77b32e539a2c7bbadcb
Anna Rodina <tavoshka@gmail.com>
2026-02-26 11:39:44 +0200

    fixed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,11 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const partnerRole = sex === 'm' ? 'wife' : 'husband';
-  const partnerText = isMarried ? `${partnerName} is my ${partnerRole}` : null;
+  const partnerRole = sex === 'm'
+    ? 'wife'
+    : 'husband';
+  const partnerText = isMarried
+    ? `${partnerName} is my ${partnerRole}`
+    : null;
 
   return (
     <section className="Person">
@@ -10,7 +14,9 @@ export const Person = ({ person }) => {
       {age !== undefined && <p className="Person__age">{`I am ${age}`}</p>}
 
       <p className="Person__partner">
-        {isMarried ? partnerText : 'I am not married'}
+        {isMarried
+          ? partnerText
+          : 'I am not married'}
       </p>
     </section>
   );
